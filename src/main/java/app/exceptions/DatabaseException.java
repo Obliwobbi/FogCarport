2cf0196a8c7e4784c6979867420be56d4a70d641
Jesper TA <Axicon89@gmail.com>
2025-11-14 08:34:39 +0100

    added mapping structure and setup dummy classes

---
+++ src/main/java/app/exceptions/DatabaseException.java
@@ -0,0 +1,15 @@
+package app.exceptions;
+import java.sql.SQLException;
+
+public class DatabaseException extends Exception
+{
+    public DatabaseException(String errorMessage)
+    {
+        super(errorMessage);
+    }
+
+    public DatabaseException(String errorMessage, SQLException exception)
+    {
+        super(errorMessage, exception);
+    }
+}
