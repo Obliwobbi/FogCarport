a72a7390ae1482b384d30a867c7dc4a2af2fe7fe
Jesper TA <Axicon89@gmail.com>
2025-11-14 08:42:39 +0100

    updated Ãpom.xml, setup main for connection pool, setup index.html

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,9 +1,38 @@
 package app;
 
+import app.config.ThymeleafConfig;
+
+import app.controllers.*;
+import app.persistence.*;
+import app.services.*;
+import io.javalin.Javalin;//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+import io.javalin.rendering.template.JavalinThymeleaf;
+
+import java.util.logging.Logger;
+
 public class Main
 {
+
+    private static final Logger LOGGER = Logger.getLogger(Main.class.getName());
+
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "postgres";
+    private static final String URL = "jdbc:postgresql://localhost:5432/%s?currentSchema=public";
+    private static final String DB = "fogcarport";
+
+    private static final ConnectionPool connectionPool = ConnectionPool.getInstance(USER, PASSWORD, URL, DB);
+
     public static void main(String[] args)
     {
+        // Initializing Javalin and Jetty webserver
 
+        Javalin app = Javalin.create(config ->
+        {
+            config.staticFiles.add("/public");
+            config.fileRenderer(new JavalinThymeleaf(ThymeleafConfig.templateEngine()));
+            config.staticFiles.add("/templates");
+        }).start(7070);
+
+        // Routing
     }
 }
