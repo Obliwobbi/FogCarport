47f3daef2e10a6763f02255ea34643b7e900c05a
obliwobbi <tobiashartzberg@gmail.com>
2025-11-17 11:08:25 +0100

    db connection

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -16,8 +16,8 @@ public class Main
     private static final Logger LOGGER = Logger.getLogger(Main.class.getName());
 
     private static final String USER = "postgres";
-    private static final String PASSWORD = "postgres";
-    private static final String URL = "jdbc:postgresql://localhost:5432/%s?currentSchema=public";
+    private static final String PASSWORD = "ModigsteFryser47";
+    private static final String URL = "jdbc:postgresql://164.92.247.68:5432/%s?currentSchema=public";
     private static final String DB = "fogcarport";
 
     private static final ConnectionPool connectionPool = ConnectionPool.getInstance(USER, PASSWORD, URL, DB);
