0b2a6cc39f4f1bf0c55cd9603ac9bed24244f7b9
Jesper TA <Axicon89@gmail.com>
2025-11-18 10:20:25 +0100

    added Create method

--- src/main/java/app/persistence/CarportMapper.java
+++ src/main/java/app/persistence/CarportMapper.java
@@ -3,10 +3,7 @@ package app.persistence;
 import app.entities.Carport;
 import app.exceptions.DatabaseException;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 
 
 public class CarportMapper
@@ -28,18 +25,29 @@ public class CarportMapper
             try (ResultSet rs = ps.executeQuery())
             {
                 if (rs.next())
-                {
-                    return new Carport(
-                            rs.getInt("carport_id"),
-                            rs.getDouble("width"),
-                            rs.getDouble("length"),
-                            rs.getDouble("height"),
-                            rs.getBoolean("with_shed"),
-                            rs.getDouble("shed_width"),
-                            rs.getDouble("shed_length"),
-                            rs.getString("customerWishes")
-                    );
-                }
+                    if (rs.getBoolean("with_shed"))
+                    {
+                        {
+                            return new Carport(
+                                    rs.getInt("carport_id"),
+                                    rs.getDouble("width"),
+                                    rs.getDouble("length"),
+                                    rs.getDouble("height"),
+                                    rs.getBoolean("with_shed"),
+                                    rs.getDouble("shed_width"),
+                                    rs.getDouble("shed_length"),
+                                    rs.getString("customerWishes"));
+                        }
+                    } else
+                    {
+                        return new Carport(
+                                rs.getInt("carport_id"),
+                                rs.getDouble("width"),
+                                rs.getDouble("length"),
+                                rs.getDouble("height"),
+                                rs.getBoolean("with_shed"),
+                                rs.getString("customerWishes"));
+                    }
                 return null;
             }
         } catch (SQLException e)
@@ -48,19 +56,74 @@ public class CarportMapper
         }
     }
 
-    public Carport createCarport() throws DatabaseException
+    public Carport createCarport(double width, double length, double height, boolean withShed, double shedWidth, double shedLength, String customerWishes) throws DatabaseException
     {
+        String sql = "INSERT INTO carport (width, length, height, with_shed, shed_width, shed_length, customer_wishes)" +
+                "VALUES(?,?,?,?,?,?,?)";
 
+        try (
+                Connection connection = connectionPool.getConnection();
+                PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS))
+        {
+            ps.setDouble(1, width);
+            ps.setDouble(2, length);
+            ps.setDouble(3, height);
+            ps.setBoolean(4, withShed);
+            ps.setString(7, customerWishes);
+
+            if (withShed)
+            {
+                ps.setDouble(5, shedWidth);
+                ps.setDouble(6, shedLength);
+            } else
+            {
+                ps.setNull(5, Types.NUMERIC);
+                ps.setNull(6, Types.NUMERIC);
+            }
+
+            int rowsAffected = ps.executeUpdate();
+
+            if (rowsAffected != 1)
+            {
+                throw new DatabaseException("Uventet fejl");
+            }
+
+            ResultSet rs = ps.getGeneratedKeys();
+
+            if (rs.next())
+            {
+                int carportId = rs.getInt(1);
+
+                if (withShed)
+                {
+                    return new Carport(carportId, width, length, height, withShed, shedWidth, shedLength, customerWishes);
+                } else
+                {
+                    return new Carport(carportId, width, length, height, withShed, customerWishes);
+                }
+            }
+        } catch (
+                SQLException e)
+        {
+            if (e.getSQLState().equals("23505")) // error code is the standard for catching unique constraint errors in PostgresSQL
+            {
+                throw new DatabaseException("carport findes allerede");
+            } else
+            {
+                throw new DatabaseException("Databasefejl ved oprettelse af carport " + e.getMessage());
+            }
+        }
+        return null;
     }
 
     public boolean updateCarport(Carport carport) throws DatabaseException
     {
-
+        return true;
     }
 
     public boolean deleteCarport(int carportId) throws DatabaseException
     {
-
+        return true;
     }
 
 
