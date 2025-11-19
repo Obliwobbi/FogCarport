9d87baa45a372aeee9b28dc4dead5bdc484e246a
Jesper TA <Axicon89@gmail.com>
2025-11-19 13:03:51 +0100

    created orders.css and setup routing in OrderController. instantiated what was necesarry in Main

--- src/main/java/app/persistence/OrderMapper.java
+++ src/main/java/app/persistence/OrderMapper.java
@@ -1,6 +1,13 @@
 package app.persistence;
 
-public class OrderMapper {
+public class OrderMapper
+{
+    private final ConnectionPool connectionPool;
 
+    public OrderMapper(ConnectionPool connectionPool)
+    {
+
+        this.connectionPool = connectionPool;
+    }
 }
 
--- src/main/java/app/services/OrderServiceImpl.java
+++ src/main/java/app/services/OrderServiceImpl.java
@@ -1,6 +1,14 @@
 package app.services;
 
-public class OrderServiceImpl implements OrderService {
+import app.persistence.OrderMapper;
 
+public class OrderServiceImpl implements OrderService
+{
+    private OrderMapper orderMapper;
+
+    public OrderServiceImpl(OrderMapper orderMapper)
+    {
+        this.orderMapper = orderMapper;
+    }
 }
 
