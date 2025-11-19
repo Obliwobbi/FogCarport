5aaf136e3ea677bf78f1d1de206f7dd5ed0a64e1
Jesper TA <Axicon89@gmail.com>
2025-11-19 13:32:55 +0100

    created empty methods for OrderMapper

--- src/main/java/app/persistence/OrderMapper.java
+++ src/main/java/app/persistence/OrderMapper.java
@@ -1,13 +1,38 @@
 package app.persistence;
 
+import app.entities.*;
+import app.exceptions.DatabaseException;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
 public class OrderMapper
 {
     private final ConnectionPool connectionPool;
 
     public OrderMapper(ConnectionPool connectionPool)
     {
-
         this.connectionPool = connectionPool;
+    }
+
+    public Order createOrder(LocalDateTime orderDate, String status, LocalDateTime deliveryDate, Drawing drawing, Carport carport, BillOfMaterials billOfMaterials) throws DatabaseException
+    {
+        return null;
+    }
+
+    public List<Order> getAllOrders() throws DatabaseException
+    {
+        return null;
+    }
+
+    public boolean updateOrderStatus(int orderId, String status) throws DatabaseException
+    {
+        return true;
+    }
+
+    public boolean deleteOrder(int orderId) throws DatabaseException
+    {
+        return true;
     }
 }
 
