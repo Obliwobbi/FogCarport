951c804bbf4b2c251194e2a6b82fa166b04d2464
Jesper TA <Axicon89@gmail.com>
2025-11-19 14:29:13 +0100

    reverted Localdate to LocalDateTime

--- src/main/java/app/entities/Order.java
+++ src/main/java/app/entities/Order.java
@@ -2,7 +2,6 @@ package app.entities;
 
 import lombok.*;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Data
@@ -13,7 +12,7 @@ public class Order
     private int orderId;
     private LocalDateTime orderDate;
     private String status; //AFVENTER ACCEPT, ACCEPTERET, BETALT
-    private LocalDate deliveryDate;
+    private LocalDateTime deliveryDate;
     private Drawing drawing;
     private Carport carport;
     private BillOfMaterials billOfMaterials;
