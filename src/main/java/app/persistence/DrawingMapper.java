b833f8231ac8691ca16b6c8531b907d7b5cb2acc
Jesper TA <Axicon89@gmail.com>
2025-11-20 11:01:36 +0100

    Updated instantiated Orderservice in Main to reflect more mappers in parameters.
    made base constructor for DrawingMapper

--- src/main/java/app/persistence/DrawingMapper.java
+++ src/main/java/app/persistence/DrawingMapper.java
@@ -1,6 +1,10 @@
 package app.persistence;
 
 public class DrawingMapper {
-
+    private ConnectionPool connectionPool;
+    public DrawingMapper(ConnectionPool connectionPool)
+    {
+        this.connectionPool = connectionPool;
+    }
 }
 
