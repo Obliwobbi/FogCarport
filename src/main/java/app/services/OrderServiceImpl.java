6d3dd3431828a8d37b93493ce4c14d658e914f5a
Jesper TA <Axicon89@gmail.com>
2025-11-20 10:30:38 +0100

    Updated constructor for OrderServiceImpl

--- src/main/java/app/services/OrderServiceImpl.java
+++ src/main/java/app/services/OrderServiceImpl.java
@@ -1,14 +1,23 @@
 package app.services;
 
-import app.persistence.OrderMapper;
+import app.entities.BillOfMaterials;
+import app.persistence.*;
 
 public class OrderServiceImpl implements OrderService
 {
     private OrderMapper orderMapper;
+    private CarportMapper carportMapper;
+    private BillOfMaterialsMapper billOfMaterialsMapper;
+    private DrawingMapper drawingMapper;
+    private CustomerMapper customerMapper;
 
-    public OrderServiceImpl(OrderMapper orderMapper)
+    public OrderServiceImpl(OrderMapper orderMapper, CarportMapper carportMapper, BillOfMaterialsMapper billOfMaterialsMapper, DrawingMapper drawingMapper, CustomerMapper customerMapper)
     {
         this.orderMapper = orderMapper;
+        this.carportMapper = carportMapper;
+        this.billOfMaterialsMapper = billOfMaterialsMapper;
+        this.drawingMapper = drawingMapper;
+        this.customerMapper = customerMapper;
     }
 }
 
