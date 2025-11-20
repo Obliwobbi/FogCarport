109804621aba81fff7c9cdc7429251a3305212d1
Jesper TA <Axicon89@gmail.com>
2025-11-20 12:29:00 +0100

    Started on OrderController, made showOrders()

--- src/main/java/app/controllers/OrderController.java
+++ src/main/java/app/controllers/OrderController.java
@@ -1,8 +1,13 @@
 package app.controllers;
 
+import app.entities.Order;
+import app.exceptions.DatabaseException;
 import app.services.OrderService;
 import io.javalin.Javalin;
 import io.javalin.http.Context;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class OrderController
 {
@@ -20,7 +25,28 @@ public class OrderController
 
     private void showOrders(Context ctx)
     {
-        ctx.render("orders.html");
+        try
+        {
+            List<Order> pendingOrders = orderService.getOrdersByStatus("AFVENTER ACCEPT");
+            List<Order> paidOrders = orderService.getOrdersByStatus("BETALT");
+            List<Order> inTransitOrders = orderService.getOrdersByStatus("AFSENDT");
+            List<Order> doneOrders = orderService.getOrdersByStatus("AFSLUTTET");
+
+            ctx.attribute("pendingOrders", pendingOrders);
+            ctx.attribute("paidOrders", paidOrders);
+            ctx.attribute("inTransitOrders", inTransitOrders);
+            ctx.attribute("doneOrders", doneOrders);
+
+            ctx.render("orders.html");
+        }
+        catch (DatabaseException e)
+        {
+            ctx.attribute("orderErrorMessage", e.getMessage());
+            ctx.attribute("paidOrders", new ArrayList<>());
+            ctx.attribute("inTransitOrders", new ArrayList<>());
+            ctx.attribute("doneOrders", new ArrayList<>());
+            ctx.redirect("/orders");
+        }
     }
 }
 
