e3861810038bf514142c77fb97446517393e3fc6
obliwobbi <tobiashartzberg@gmail.com>
2025-11-24 11:35:16 +0100

    created test class for calculator method for posts

---
+++ src/test/java/app/services/MaterialListServiceImplTest.java
@@ -0,0 +1,22 @@
+package app.services;
+
+import app.entities.Carport;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MaterialListServiceImplTest
+{
+
+    @BeforeEach
+    void setUp()
+    {
+    }
+
+    @Test
+    void calculatePosts()
+    {
+        Carport carport = new Carport(1)
+    }
+}
