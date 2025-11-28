d15b009389f067cd976764e607d03b3f8982180b
Jesper TA <Axicon89@gmail.com>
2025-11-28 08:42:54 +0100

    Added an updated materials.sql script that should work with calculator method

---
+++ src/main/resources/sql/materials.sql
@@ -0,0 +1,53 @@
+INSERT INTO materials (name, description, unit, unit_type, material_length, material_width, material_height, price)
+VALUES ('25x200 mm trykimpr. Brædt', 'Understernbrædder', 1, 'stk', 360, 200, 25, 79.00),
+       ('25x200 mm trykimpr. Brædt', 'Understernbrædder', 1, 'stk', 540, 200, 25, 118.00),
+
+       ('25x125 mm trykimpr. Brædt', 'Oversternbrædder', 1, 'stk', 360, 125, 25, 48.00),
+       ('25x125 mm trykimpr. Brædt', 'Oversternbrædder', 1, 'stk', 540, 125, 25, 72.00),
+
+       ('38x73 mm Lægte ubeh.', 'Z til bagside af dør', 1, 'stk', 420, 73, 38, 29.00),
+
+       ('45x95 mm Reglar ubh.', 'Løsholter til skur gavle', 1, 'stk', 270, 95, 45, 32.00),
+       ('45x95 mm Reglar ubh.', 'Løsholter til skur sider', 1, 'stk', 240, 95, 45, 28.00),
+
+       ('45x195 mm Spærtræ ubh.', 'Remme i sider – sadles ned i stolper', 1, 'stk', 600, 195, 45, 115.00),
+       ('45x195 mm Spærtræ ubh.', 'Remme i sider – skur del, deles', 1, 'stk', 480, 195, 45, 96.00),
+
+       ('45x195 mm Spærtræ ubh.', 'Spær, monteres på rem', 1, 'stk', 600, 195, 45, 115.00),
+
+       ('97x97 mm trykimpr. Stolpe', 'Stolper nedgraves 90 cm', 1, 'stk', 300, 97, 97, 129.00),
+
+       ('19x100 mm trykimpr. Brædt', 'Beklædning af skur 1 på 2', 1, 'stk', 210, 100, 19, 18.00),
+
+       ('19x100 mm trykimpr. Brædt', 'Vandbrædt på stern', 1, 'stk', 540, 100, 19, 54.00),
+       ('19x100 mm trykimpr. Brædt', 'Vandbrædt på stern', 1, 'stk', 360, 100, 19, 36.00),
+
+       ('Plastmo Ecolite blåtonet', 'Tagplader monteres på spær', 1, 'stk', 600, NULL, NULL, 159.00),
+       ('Plastmo Ecolite blåtonet', 'Tagplader monteres på spær', 1, 'stk', 360, NULL, NULL, 109.00),
+
+-- HARDWARE / BESLAG / SKRUER
+       ('Plastmo bundskruer', 'Skruer til tagplader', 200, 'pakke', NULL, NULL, NULL, 129.00),
+
+       ('Hulbånd 1x20 mm', 'Til vindkryds på spær', 1, 'rulle', NULL, NULL, NULL, 49.00),
+
+       ('Universal beslag højre 190 mm', 'Til montering af spær på rem', 1, 'stk', NULL, NULL, NULL, 12.00),
+
+       ('Universal beslag venstre 190 mm', 'Til montering af spær på rem', 1, 'stk', NULL, NULL, NULL, 12.00),
+
+       ('Bræddebolt 10x120 mm', 'Til montering af rem på stolper', 1, 'stk', NULL, NULL, NULL, 4.50),
+
+       ('Firkantskiver 40x40x11 mm', 'Til montering af rem på stolper', 1, 'stk', NULL, NULL, NULL, 1.50),
+
+       ('Beslagskruer 4.0x50 mm', 'Til montering af universalbeslag + hulbånd', 250, 'pakke', NULL, NULL, NULL, 39.00),
+
+       ('Skruer 4.5x50 mm', 'Til montering af inderste beklædning', 300, 'pakke', NULL, NULL, NULL, 49.00),
+
+       ('Skruer 4.5x60 mm', 'Til montering af stern & vandbrædt', 200, 'pakke', NULL, NULL, NULL, 45.00),
+
+       ('Skruer 4.5x70 mm', 'Til montering af yderste beklædning', 400, 'pakke', NULL, NULL, NULL, 59.00),
+
+       ('Stalddørsgreb 50x75 mm', 'Lås til skurdør', 1, 'sæt', NULL, NULL, NULL, 89.00),
+
+       ('T-hængsel 390 mm', 'Til skurdør', 1, 'stk', NULL, NULL, NULL, 35.00),
+
+       ('Vinkelbeslag 3 mm', 'Til montering af løsholter i skur', 1, 'stk', NULL, NULL, NULL, 5.00);
