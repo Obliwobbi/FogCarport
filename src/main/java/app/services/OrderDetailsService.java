26089b4f26d4e823aa4e1c1107d56f01f9b0b9ec
obliwobbi <tobiashartzberg@gmail.com>
2025-11-29 21:49:00 +0100

    Made the method throw an exception

--- src/main/java/app/services/OrderDetailsService.java
+++ src/main/java/app/services/OrderDetailsService.java
@@ -2,10 +2,11 @@ package app.services;
 
 import app.entities.Carport;
 import app.entities.MaterialsLine;
+import app.exceptions.DatabaseException;
 
 import java.util.List;
 
 public interface OrderDetailsService
 {
-    List<MaterialsLine> createMaterialList(Carport carport);
+    List<MaterialsLine> createMaterialList(Carport carport) throws DatabaseException;
 }
