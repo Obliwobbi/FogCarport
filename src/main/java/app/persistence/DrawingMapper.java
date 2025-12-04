45e3d431842fe9138c6bffd748bcb986ffddcb91
Jesper TA <Axicon89@gmail.com>
2025-12-04 12:37:50 +0100

    added a get and create method for drawingMapper

--- src/main/java/app/persistence/DrawingMapper.java
+++ src/main/java/app/persistence/DrawingMapper.java
@@ -1,17 +1,74 @@
 package app.persistence;
 
 import app.entities.Drawing;
+import app.exceptions.DatabaseException;
 
-public class DrawingMapper {
+import java.sql.*;
+
+public class DrawingMapper
+{
     private ConnectionPool connectionPool;
+
     public DrawingMapper(ConnectionPool connectionPool)
     {
         this.connectionPool = connectionPool;
     }
 
-    public Drawing getDrawingById(int drawingId)
+    public Drawing createDrawing(String svgData) throws DatabaseException
     {
+        String sql = "INSERT INTO drawings (drawing_data)" +
+                " VALUES (?)";
+
+        try (Connection connection = connectionPool.getConnection();
+             PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS))
+        {
+            ps.setString(1, svgData);
+
+            int rowsAffected = ps.executeUpdate();
+
+            if (rowsAffected != 1)
+            {
+                throw new DatabaseException("Uventet fejl");
+            }
+
+            ResultSet rs = ps.getGeneratedKeys();
+
+            if (rs.next())
+            {
+                int drawingId = rs.getInt(1);
+                return new Drawing(drawingId, svgData);
+            }
+        }
+        catch (SQLException e)
+        {
+            throw new DatabaseException("Database fejl ved opretetelse af Tegning" + e.getMessage());
+        }
         return null;
+    }
+
+    public Drawing getDrawingById(int drawingId) throws DatabaseException
+    {
+        String sql = "SELECT * FROM drawings WHERE drawing_id= ?";
+
+        try (Connection connection = connectionPool.getConnection();
+             PreparedStatement ps = connection.prepareStatement(sql))
+        {
+            ps.setInt(1, drawingId);
+            try (ResultSet rs = ps.executeQuery())
+            {
+                if (rs.next())
+                {
+                    return new Drawing(
+                            rs.getInt("drawing_id"),
+                            rs.getString("drawing_data"));
+                }
+                throw new DatabaseException("der blev ikke fundet en tegning med id: " + drawingId);
+            }
+        }
+        catch (SQLException e)
+        {
+            throw new DatabaseException("Fejl ved hentning af data for tegning" + e);
+        }
     }
 }
 
