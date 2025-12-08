2c684e84a1c74461160f5ac968ec41c89e909215
obliwobbi <tobiashartzberg@gmail.com>
2025-12-08 20:32:39 +0100

    implemented updateDrawing method to drawingService

--- src/main/java/app/services/DrawingService.java
+++ src/main/java/app/services/DrawingService.java
@@ -8,6 +8,8 @@ public interface DrawingService
 {
     Drawing createDrawing(Drawing drawing) throws DatabaseException;
 
+    void updateDrawing(Drawing drawing) throws DatabaseException;
+
     void deleteDrawing(int drawingId) throws DatabaseException;
 
     String showDrawing(Carport carport, CalculatorService calculatorService);
