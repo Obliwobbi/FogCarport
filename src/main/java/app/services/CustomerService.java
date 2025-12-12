c556ca0148692db944024f072e254f9d334781b0
Jesper TA <Axicon89@gmail.com>
2025-12-12 10:08:26 +0100

    added private helper methods with validation logic for all input values in ContactController

--- src/main/java/app/services/CustomerService.java
+++ src/main/java/app/services/CustomerService.java
@@ -5,17 +5,15 @@ import app.exceptions.DatabaseException;
 
 public interface CustomerService {
 
-    public Customer registerNewCustomer(String firstName, String lastName, String email, String phoneNumber, String street, String houseNumber, int zipcode, String city) throws DatabaseException;
+    Customer registerNewCustomer(String firstName, String lastName, String email, String phoneNumber, String street, String houseNumber, int zipcode, String city) throws DatabaseException;
 
-    public Customer findCustomerByName(String name) throws DatabaseException;
+    Customer findCustomerByName(String name) throws DatabaseException;
 
-    public Customer findCustomerById(int id) throws DatabaseException;
+    Customer findCustomerById(int id) throws DatabaseException;
 
-    public Customer findCustomerByEmail(String email) throws DatabaseException;
+    Customer findCustomerByEmail(String email) throws DatabaseException;
 
-    public Customer findCustomerByPhone(String phone) throws DatabaseException;
+    Customer findCustomerByPhone(String phone) throws DatabaseException;
 
     void deleteCustomer(int customerId) throws DatabaseException;
-
-    String validateEmail(String email);
 }
