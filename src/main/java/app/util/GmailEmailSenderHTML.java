ad7ab3c6a73504e37e94e77a3188a5278e1d155c
Jesper TA <Axicon89@gmail.com>
2025-12-17 12:47:00 +0100

    updated smptp to system env instead of hardcoded smtp gmail

--- src/main/java/app/util/GmailEmailSenderHTML.java
+++ src/main/java/app/util/GmailEmailSenderHTML.java
@@ -42,10 +42,11 @@ public class GmailEmailSenderHTML
     public void sendHtmlEmail(String to, String subject, String htmlBody) throws MessagingException
     {
         Properties props = new Properties();
-        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.host", System.getenv("MAIL_SMTP_HOST"));
+        props.put("mail.smtp.port", System.getenv("MAIL_SMTP_PORT"));
         props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.smtp.host", "smtp.gmail.com");
-        props.put("mail.smtp.port", "587");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.ssl.trust", System.getenv("MAIL_SMTP_HOST"));
 
         Session session = Session.getInstance(props, new Authenticator()
         {
